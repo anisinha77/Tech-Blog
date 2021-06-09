c8ac2612e928c17be9b9aa30d7040209bfa81d68
anisinha77 <ak726556@gmail.com>
2021-06-09 18:58:58 +0530

    Designed Post schema and category schema.

---
+++ api/Post.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const PostSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        photo: {
+            type: String,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        categories: {
+            type: Array,
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Post = mongoose.model("Post", PostSchema)
+
+export default Post
--- api/User.js
+++ api/User.js
@@ -1,25 +1,26 @@
 import mongoose from 'mongoose'
 
-const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profilePic: {
+            type: String,
+            default: "",
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    profilePic: {
-        type: String,
-        default: "",
-    },
-},
     { timestamps: true }
 );
 
