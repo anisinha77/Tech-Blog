3679e6ef65653afcf03ce8f8f5a4df516106b821
anisinha77 <ak726556@gmail.com>
2021-06-09 18:20:28 +0530

    Created mongodb cluster and established connection through mongoose.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB();
+
 const app = express()
+
 
 app.listen('5000', () => {
     console.log("Listening to server 5000");
