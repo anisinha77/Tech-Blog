1ee41c0035500c0236458791432cfbca65781cf9
anisinha77 <ak726556@gmail.com>
2021-06-10 01:58:50 +0530

    Created authentication routes to validate user login and creation of new user.

--- api/index.js
+++ api/index.js
@@ -2,12 +2,17 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import connectDB from './config/db.js'
+import authRoute from './routes/auth.js'
 
+const app = express()
 dotenv.config()
+
+app.use(express.json())
 
 connectDB();
 
-const app = express()
+
+app.use('/api/auth', authRoute)
 
 
 app.listen('5000', () => {
