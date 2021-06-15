5ba413b71e4b12beddbc2976c4e9d985bc0f6677
anisinha77 <ak726556@gmail.com>
2021-06-15 20:30:12 +0530

    Update post in nodejs server.

--- client/src/components/PostScreenComponent/index.js
+++ client/src/components/PostScreenComponent/index.js
@@ -13,6 +13,11 @@ const Index = () => {
     const { user } = useContext(Context)
     const PF = "http://localhost:5000/images/"
 
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [updateMode, setUpdateMode] = useState(false)
+
+
     let location = useLocation();
     const path = location.pathname.split("/")[2]
     // console.log(path)
@@ -24,10 +29,14 @@ const Index = () => {
             const res = await axios.get("/posts/" + path)
             // console.log(res)
             setpost(res.data)
+            setTitle(res.data.title)
+            setDescription(res.data.description)
         }
         fetchpost()
     }, [path])
 
+
+    //delete post
     const handleDelete = async () => {
         try {
             await axios.delete(`/posts/${post._id}`, {
@@ -40,29 +49,56 @@ const Index = () => {
         }
     }
 
+    //update post 
+    const handleEdit = (e) => {
+        setUpdateMode(true)
+    }
+    const handleUpdate = async () => {
+        try {
+            await axios.put(`/posts/${post._id}`, {
+                username: user.username, title, description
+            })
+            // setUpdateMode(false)
+            window.location.reload()
+        } catch (error) {
+
+        }
+    }
+
     return (
         <div className="post-screen">
             <div className="post-container">
                 {post.photo &&
                     (<img className="post-image" src={PF + post.photo} alt="Post Blog" />)}
-                <h2 className="post-title">
-                    <strong>{post.title}</strong>
-                    {post.username === user?.username &&
 
-                        <div className="edit-delete">
-                            <span className="edit-icon"><i className="far fa-edit"></i></span>
-                            <span className="delete-icon"><i className="far fa-trash-alt" onClick={handleDelete}></i></span>
-                        </div>
-                    }
-                </h2>
+                {
+                    updateMode ? <input type="text" value={title} className="title-input" autoFocus onChange={(e) => setTitle(e.target.value)} /> :
+                        (<h2 className="post-title">
+                            <strong>{post.title}</strong>
+                            {post.username === user?.username &&
+
+                                <div className="edit-delete">
+                                    <span className="edit-icon" onClick={handleEdit}><i className="far fa-edit"></i></span>
+                                    <span className="delete-icon"><i className="far fa-trash-alt" onClick={handleDelete}></i></span>
+                                </div>
+                            }
+                        </h2>)
+                }
                 <div className="info">
                     <div className="post-details">
                         <div>Author : <Link className="to-link" to={`/?user=${post.username}`}>{post.username}</Link></div>
                         <div>Created : {new Date(post.createdAt).toDateString()}</div>
                     </div>
-                    <div className="description">
-                        {post.description}
-                    </div>
+                    {
+                        updateMode ? <textarea className="desc-input" value={description} onChange={(e) => setDescription(e.target.value)}></textarea> :
+                            (<div className="description">
+                                {post.description}
+                            </div>)
+                    }
+                    {
+                        updateMode &&
+                        <button className="update-button" onClick={handleUpdate}>Update</button>
+                    }
                 </div>
             </div>
         </div>
